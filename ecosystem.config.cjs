e5d1601889af66ce0b9d8db3d95535151428ce5b
Rizkal <rixzkl.lamaau@gmail.com>
2026-06-26 19:06:33 +0700

    fix: deployment

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "laporan-api",
-      cwd: "/www/wwwroot/rizkal.laporan.space/apps/api",
+      cwd: "/www/wwwroot/laporan.rizkal.space/apps/api",
       script: "dist/index.js",
       instances: 1,
       exec_mode: "fork",
